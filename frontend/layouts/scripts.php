830a4b3be0706b47f4db2ec03309550871297b3e
STUDENT <quynhb1706634@ctu.edu.vn>
2020-09-19 19:24:24 +0700

    frontend_contact

---
+++ frontend/layouts/scripts.php
@@ -0,0 +1,3 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/da/assets/vendor/jquery.js"></script>
+<script src="/da/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
---
+++ frontend/layouts/styles.php
@@ -0,0 +1,6 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/da/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
+<!-- Font awesome -->
+<link rel="stylesheet" href="/da/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
+<!-- Custom css - Các file css do chúng ta tự viết -->
+<link rel="stylesheet" href="/da/assets/frontend/css/style.css" type="text/css" />
